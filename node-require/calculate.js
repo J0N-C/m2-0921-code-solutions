902eb52aee57481b19b21cec762edefe0b9b25ff
Jonathan Chiang <jc91765@gmail.com>
2021-11-04 20:04:47 -0700

    completed node-require

---
+++ node-require/calculate.js
@@ -0,0 +1,18 @@
+const add = require('./add');
+const subtract = require('./subtract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[4]);
+if (process.argv[3] === 'plus') {
+  console.log(`result: ${add(a, b)}`);
+}
+if (process.argv[3] === 'minus') {
+  console.log(`result: ${subtract(a, b)}`);
+}
+if (process.argv[3] === 'times') {
+  console.log(`result: ${multiply(a, b)}`);
+}
+if (process.argv[3] === 'over') {
+  console.log(`result: ${divide(a, b)}`);
+}
