39ecd55b29db113d5b0f5f24c5757cfea6ae5fbc
Jonathan Chiang <jc91765@gmail.com>
2021-11-08 20:54:00 -0800

    completed user-can-delete

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const methodName = process.argv[2];
+const idNum = parseInt(process.argv[3]);
 
 if (methodName === 'read') {
   fs.readFile('data.json', 'utf8', (err, data) => {
@@ -11,12 +12,23 @@ if (methodName === 'read') {
   });
 }
 
-if (methodName === 'create') {
+if (methodName === 'create' && typeof process.argv[3] === 'string') {
   fs.readFile('data.json', 'utf8', (err, data) => {
     if (err) throw err;
     const dataObj = JSON.parse(data);
     dataObj.notes[dataObj.nextId] = process.argv[3];
     dataObj.nextId++;
+    fs.writeFile('data.json', JSON.stringify(dataObj, null, 2), 'utf8', err => {
+      if (err) throw err;
+    });
+  });
+}
+
+if (methodName === 'delete' && idNum !== 'NaN') {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const dataObj = JSON.parse(data);
+    delete dataObj.notes[idNum];
     fs.writeFile('data.json', JSON.stringify(dataObj, null, 2), 'utf8', err => {
       if (err) throw err;
     });
