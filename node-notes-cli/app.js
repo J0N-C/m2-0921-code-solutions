a255f0a27341bb74b4febe68cdcd4158639ad3e2
Jonathan Chiang <jc91765@gmail.com>
2021-11-09 19:51:57 -0800

    completed node-notes-cli

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const methodName = process.argv[2];
-const idNum = parseInt(process.argv[3]);
+const textString = process.argv[3];
+const idNum = parseInt(textString);
+const updatedNote = process.argv[4];
 
-if (methodName === 'read') {
+const read = () => {
   fs.readFile('data.json', 'utf8', (err, data) => {
     if (err) throw err;
     const notes = JSON.parse(data).notes;
@@ -10,27 +12,54 @@ if (methodName === 'read') {
       console.log(`${key}: ${value}`);
     }
   });
+};
+
+if (methodName === 'read') {
+  read();
 }
 
-if (methodName === 'create' && typeof process.argv[3] === 'string') {
+const create = textString => {
   fs.readFile('data.json', 'utf8', (err, data) => {
     if (err) throw err;
     const dataObj = JSON.parse(data);
-    dataObj.notes[dataObj.nextId] = process.argv[3];
+    dataObj.notes[dataObj.nextId] = textString;
     dataObj.nextId++;
-    fs.writeFile('data.json', JSON.stringify(dataObj, null, 2), 'utf8', err => {
-      if (err) throw err;
-    });
+    updateEntries(dataObj);
   });
+};
+
+if (methodName === 'create' && typeof textString === 'string') {
+  create(textString);
 }
+
+const deleteNote = indexNum => {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const dataObj = JSON.parse(data);
+    delete dataObj.notes[indexNum];
+    updateEntries(dataObj);
+  });
+};
 
 if (methodName === 'delete' && idNum !== 'NaN') {
+  deleteNote(idNum);
+}
+
+const updateNote = (indexNum, newNote) => {
   fs.readFile('data.json', 'utf8', (err, data) => {
     if (err) throw err;
     const dataObj = JSON.parse(data);
-    delete dataObj.notes[idNum];
-    fs.writeFile('data.json', JSON.stringify(dataObj, null, 2), 'utf8', err => {
-      if (err) throw err;
-    });
+    dataObj.notes[indexNum] = newNote;
+    updateEntries(dataObj);
   });
+};
+
+if (methodName === 'update' && idNum !== 'NaN' && typeof process.argv[4] === 'string') {
+  updateNote(idNum, updatedNote);
 }
+
+const updateEntries = data => {
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), 'utf8', err => {
+    if (err) throw err;
+  });
+};
