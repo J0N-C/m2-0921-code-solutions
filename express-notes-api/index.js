7060c4af3e29e3d83fbd97024f521cacb85a8a57
Jonathan Chiang <jc91765@gmail.com>
2021-11-18 21:03:33 -0800

    completed client can get single note by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -20,5 +20,21 @@ app.get('/api/notes', function (req, res) {
   });
 });
 
+app.get('/api/notes/:id', function (req, res) {
+  const idNum = parseInt(req.params.id);
+  if (isNaN(idNum) || idNum < 1) {
+    return res.status(400).send({ error: 'id must be a positive integer' });
+  }
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const obj = JSON.parse(data);
+    if (obj.notes[idNum] === undefined) {
+      return res.status(404).send({ error: `cannot find note with id ${idNum}` });
+    }
+    return res.status(200).send(obj.notes[idNum]);
+  });
+});
+
 app.listen(3000, () => {
+
 });
